956fcf06ab3ba901ee12945a2ec5c1b232ce90b5
Your Name <you@example.com>
2023-07-18 00:47:26 -0400

    Task 2: Editable budget value

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,19 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, setBudget  } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £ <input
+                        min = "2000" max="20000" step="10"
+                        type='number'
+                        id='budget'
+                        
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input>
+</span>
         </div>
     );
 };
